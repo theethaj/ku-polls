73c9804799e54d69c8efeb0b7d88e801beb8893d
Tetach Rattanavikran <47658385+theethaj@users.noreply.github.com>
2020-09-08 18:43:45 +0700

    Complete tutorial Part 1-7

--- polls/views.py
+++ polls/views.py
@@ -22,9 +22,6 @@ class DetailView(generic.DetailView):
     template_name = 'polls/detail.html'
 
     def get_queryset(self):
-        """
-        Excludes any questions that aren't published yet.
-        """
         return Question.objects.filter(pub_date__lte=timezone.now())
 
 
